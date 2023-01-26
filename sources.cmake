15938ced05b634bb332f02edea3d5ff35fe7a357
Kyunghwan Kwon <k@mononn.com>
2023-01-26 16:36:19 +0900

    Fix CMake working directory

--- sources.cmake
+++ sources.cmake
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: MIT
 
 if(TARGET_PLATFORM STREQUAL esp32)
-	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/esp32.c)
+	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/esp32.c)
 elseif(TARGET_PLATFORM STREQUAL zephyr)
-	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/zephyr.c)
+	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/zephyr.c)
 else()
 	message(WARN "No target specified. Building with dummy target")
-	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/dummy.c)
+	list(APPEND PWIFI_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/dummy.c)
 endif()
 
-list(APPEND PWIFI_INCS ${CMAKE_CURRENT_SOURCE_DIR}/include)
+list(APPEND PWIFI_INCS ${CMAKE_CURRENT_LIST_DIR}/include)
